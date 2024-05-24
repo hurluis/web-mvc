0768ea69039017e5765696f30f698e8826174c76
William Velasquez <wvelasquez@udemedellin.edu.co>
2024-05-24 09:37:37 -0500

    Separacion en Blueprints

--- app.py
+++ app.py
@@ -5,38 +5,17 @@ from flask import Flask, request, jsonify , url_for
 # Para poder servir plantillas HTML desde archivos, es necesario importar el modulo render_template
 from flask import render_template
 
-from src.model.Usuario import Usuario
-import src.controller.ControladorUsuarios as ControladorUsuarios
+import sys
+sys.path.append("src")
+
+from view_web import vista_usuarios 
 
 # Flask constructor: crea una variable que nos servirá para comunicarle a Flask
 # la configuración que queremos para nuestra aplicación
 app = Flask(__name__)     
 
-@app.route("/")
-def Home():
-   return render_template("index.html")
+app.register_blueprint( vista_usuarios.blueprint )
 
-@app.route("/usuario")
-def buscar_usuario():
-   # request.args es un diccionario que contiene los parametros en le URL solicitada
-   cedula = request.args["cedula"]
-   usuario = ControladorUsuarios.BuscarPorCedula(cedula)
-   return render_template("usuario.html", user = usuario )
-
-@app.route("/nuevo-usuario")
-def formulario_nuevo_usuario():
-   return render_template("new-user.html")
-
-@app.route("/crear-usuario")
-def crear_usuario():
-   cedula = request.args["cedula"]
-   nombre=request.args["nombre"]
-   apellido=request.args["apellido"]
-   usuario_prueba = Usuario( cedula=cedula, nombre=nombre, apellido=apellido, correo="no@tiene.correo", 
-                            direccion=request.args["direccion"], telefono=request.args["telefono"], 
-                            codigo_departamento=request.args["codigo_departamento"], codigo_municipio=request.args["codigo_municipio"]  ) 
-   ControladorUsuarios.Insertar( usuario_prueba )
-   return "Aqui se va a crear un usuario"
 
 # Esta linea permite que nuestra aplicación se ejecute individualmente
 if __name__=='__main__':
---
+++ src/view_web/vista_usuarios.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, render_template, request
+
+blueprint = Blueprint( "vista_usuarios", __name__, "templates" )
+
+import sys
+sys.path.append("src")
+from model.Usuario import Usuario
+import controller.ControladorUsuarios as ControladorUsuarios
+
+@blueprint.route("/")
+def Home():
+   return render_template("index.html")
+
+@blueprint.route( "/nuevo" )
+def nuevo():
+   return render_template("new-user.html")
+
+@blueprint.route( "/crear-usuario")
+def crear_usuario():
+   usuario = Usuario( cedula=request.args["cedula"], apellido=request.args["apellido"], nombre=request.args["nombre"], correo=request.args["correo"], direccion=request.args["direccion"], telefono=request.args["telefono"], codigo_departamento=request.args["codigo_departamento"], codigo_municipio=request.args["codigo_municipio"] ) 
+   ControladorUsuarios.Insertar( usuario )
+
+   return render_template("usuario.html", user = usuario, mensaje = "Usuario insertado exitosamente!" )
+
+@blueprint.route("/usuario")
+def buscar_usuario():
+   # request.args es un diccionario que contiene los parametros en le URL solicitada
+   cedula = request.args["cedula"]
+   usuario = ControladorUsuarios.BuscarPorCedula(cedula)
+   return render_template("usuario.html", user = usuario )
